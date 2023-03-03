dd2860a71131e4b01683089c4e49211c78280ddd
Lisa <lisabazdyreva@gmail.com>
2023-03-03 14:25:04 +0300

    init commit

---
+++ script.js
@@ -0,0 +1,152 @@
+
+const dataTable = document.querySelector('.data-table').querySelector('tbody');
+
+const addDataButton = document.querySelector('.action-button--add');
+const newRow = document.querySelector('#new-row').content.querySelector('.data-table__body-row');
+
+const removeButtons = document.querySelectorAll('.remove-data-button');
+
+const postDataButton = document.querySelector('.action-button--post');
+
+
+const validity = {
+  isNameValid: false,
+  isJobTitleValid: false,
+  isAgeValid: false,
+  isCompetenciesValid: false,
+};
+
+
+//2. Валидировать ввод
+
+// имя
+newRow.querySelector('#name').addEventListener('input', (evt) => {
+    if (evt.target.value.trim()) {
+      validity.isNameValid = true;
+    }
+
+  if (validity.isNameValid && validity.isAgeValid  && validity.isCompetenciesValid) {
+    newRow.querySelector('.save-data-button').disabled = false;
+  }
+});
+
+
+// возраст
+newRow.querySelector('#age').addEventListener('input', (evt) => {
+  if (Number(evt.target.value)) {
+    validity.isAgeValid = true;
+  }
+
+  if (validity.isNameValid && validity.isAgeValid  && validity.isCompetenciesValid) {
+    newRow.querySelector('.save-data-button').disabled = false;
+  }
+});
+
+// компетенции
+newRow.querySelector('#competencies').addEventListener('input', (evt) => {
+  if (evt.target.value.trim()) {
+    validity.isCompetenciesValid = true;
+  }
+
+  if (validity.isNameValid && validity.isAgeValid  && validity.isCompetenciesValid) {
+    newRow.querySelector('.save-data-button').disabled = false;
+  }
+});
+
+
+
+
+//1. Добавлять новую строку, если не открыта уже строка
+
+addDataButton.addEventListener('click', () => {
+  addDataButton.disabled = true;
+  postDataButton.disabled = true;
+
+  if (document.querySelector('.data-table__body-row--new')) {
+    return;
+  }
+
+  dataTable.append(newRow);
+  newRow.querySelector('.save-data-button').disabled = true;
+  // обработчики ввода
+
+});
+
+
+
+//4. По кнопке добавить строку в таблицу
+newRow.querySelector('.save-data-button').addEventListener('click', () => {
+  const name = newRow.querySelector('#name').value;
+  // const jobTitle = newRow
+  const age = newRow.querySelector('#age').value;
+  const competencies = newRow.querySelector('#competencies').value;
+
+  const tdName = document.createElement('td');
+  tdName.textContent = name;
+
+  const tdJobTitle = document.createElement('td');
+  tdJobTitle.textContent = 'Аналитик';
+
+  const tdAge = document.createElement('td');
+  tdAge.textContent = age;
+
+  const tdCompetencies = document.createElement('td');
+  tdCompetencies.textContent = competencies;
+
+  newRow.remove();
+
+  const tdRemoveButton = document.createElement('td');
+  tdRemoveButton.innerHTML = `<button class="remove-data-button" type="button">Del</button>`;
+
+  const trNew = document.createElement('tr');
+  trNew.classList.add('data-table__body-row');
+
+  const idForRow = dataTable.querySelectorAll('tr').length;
+  trNew.id = `${idForRow }-row`;
+
+  trNew.append(tdName);
+  trNew.append(tdJobTitle);
+  trNew.append(tdAge);
+  trNew.append(tdCompetencies);
+  trNew.append(tdRemoveButton);
+
+  dataTable.append(trNew);
+  addDataButton.disabled = false;
+  postDataButton.disabled = false;
+});
+
+
+//3. По кнопке удалять строку
+removeButtons.forEach((removeButton) => {
+  removeButton.addEventListener('click', (evt) => {
+    const rowToRemove = evt.target.parentNode.parentNode;
+
+    if (rowToRemove) {
+      rowToRemove.remove();
+    }
+  });
+})
+
+
+//5. Отправить джейсон с полями
+
+postDataButton.addEventListener('click', () => {
+  const rows = dataTable.querySelectorAll('tr');
+
+  const dataObject = {};
+
+  rows.forEach((row) => {
+    const dataRow = {};
+
+    dataRow['name'] = row.children[0].innerText;
+    dataRow['jobTitle'] = row.children[1].innerText;
+    dataRow['age'] = Number(row.children[2].innerText);
+    dataRow['competencies'] = row.children[3].innerHTML;
+
+    dataObject[parseInt(row.id)] = dataRow;
+  });
+
+  const jsonData = JSON.stringify(dataObject);
+
+  console.log(jsonData);
+});
